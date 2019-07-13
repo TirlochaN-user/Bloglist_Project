3a3bf452d69a137608910cc55dc55656da51665a
champsun <sunny.trilochan@gmail.com>
2019-07-14 01:32:15 +0530

    delete icon added with functionalities to bloglist.html

--- js/bloglist.js
+++ js/bloglist.js
@@ -1,7 +1,24 @@
 var postList=document.getElementsByClassName("post-desc");
-var trashhtml='<i class="fa fa-trash" id="trashicon"></i>';
+var trashhtml='<button onclick="showDeleteModal(1)"><i class="fa fa-trash" id="trashicon"></i></button>';
 var ellipsishtml='<a href="post.html"><i class="fa fa-ellipsis-h" id="ellipicon"></i></a>';
 for(var i=0;i<postList.length;i++)
 {
     postList[i].innerHTML+=trashhtml+ellipsishtml;
+}
+
+function showDeleteModal(x)
+{
+    var modalhtml=document.getElementById('deletemodal');
+    if(x==0)
+        modalhtml.setAttribute('style','display:none');
+    else
+        modalhtml.style.display='block';
+}
+showDeleteModal(0);
+function closeDeleteModal()
+{
+    var modalelement=document.getElementById('deletemodal');
+    var body=document.querySelector('body');
+    body.style.display='';
+    modalelement.style.display='none';
 }
